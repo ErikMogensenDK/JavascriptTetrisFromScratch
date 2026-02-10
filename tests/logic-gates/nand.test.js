f2b83358aea47433723ec6be4b5d2d404a4826bb
ErikMogensenDK <erikmogensen1@gmail.com>
2026-02-10 21:14:59 +0100

    changed naming for uniformity

--- tests/logic-gates/nand.test.js
+++ tests/logic-gates/nand.test.js
@@ -1,13 +1,13 @@
 import { nand } from '../../logic-gates/nand.js';
 
-function nandTest(aBit, bBit, expectedResult) {
+function testNand(aBit, bBit, expectedResult) {
   describe('Nand test', () => {
     test(`${aBit} and ${bBit} should return ${expectedResult}`, () =>
       expect(nand(aBit, bBit)).toBe(expectedResult))
   }
   )
 }
-nandTest(0, 0, 1)
-nandTest(1, 0, 1)
-nandTest(0, 1, 1)
-nandTest(1, 1, 0)
+testNand(0, 0, 1)
+testNand(1, 0, 1)
+testNand(0, 1, 1)
+testNand(1, 1, 0)
