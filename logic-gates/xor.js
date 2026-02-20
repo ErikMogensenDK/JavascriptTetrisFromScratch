d40449e56c7ac895f5ddc5c65351865ff51615c3
ErikMogensenDK <erikmogensen1@gmail.com>
2026-02-20 16:50:35 +0100

    implemented test and first attempt at xor

--- logic-gates/xor.js
+++ logic-gates/xor.js
@@ -1,8 +1,12 @@
 import {and} from './and.js'
 import {not} from './not.js'
+import {or} from './or.js'
 
 export function xor(aBit, bBit) {
-	return(and(not(aBit), bBit))
+	const tmp1 = and(not(aBit), bBit)
+	const tmp2 = and(not(bBit), aBit)
+
+	return(or(tmp1,tmp2))
 }
 
 export default xor;
