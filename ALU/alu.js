77b52750ee09438d5f8b03246bc1eb5e4858b503
ErikMogensenDK <erikmogensen1@gmail.com>
2026-03-24 07:49:22 +0100

    removed logging for debugging

--- ALU/alu.js
+++ ALU/alu.js
@@ -8,33 +8,22 @@ export function alu(x16, y16, zx, nx, zy, ny, f, no){
 	const zeroInput = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 	const potentiallyZeroedX16 = mux16(x16, zeroInput, zx);
-	console.log("potentiallyZeroedX16", potentiallyZeroedX16)
 	const potentiallyZeroedY16 = mux16(y16, zeroInput, zy);
-	console.log("potentiallyZeroedY16", potentiallyZeroedY16)
 
 	const notX16Result = not16(potentiallyZeroedX16);
-	console.log("notX16Result", notX16Result)
 	const notOrZeroX = mux16(potentiallyZeroedX16, notX16Result, nx);
-	console.log("notOrZeroX", notOrZeroX)
 
 	const notY16Result = not16(potentiallyZeroedY16);
-	console.log("notY16Result", notY16Result)
 	const notOrZeroY = mux16(potentiallyZeroedY16, notY16Result, ny);
-	console.log("notOrZeroY", notOrZeroY)
 
 
 	const xPlusY = add16(notOrZeroX, notOrZeroY);
-	console.log("xPlusY", xPlusY)
 	const xAndY = and16(notOrZeroX, notOrZeroY);
-	console.log("xAndY", xAndY)
 
 	const fResult = mux16(xAndY, xPlusY, f);
-	console.log("fResult", fResult)
 	const notFResult = not16(fResult);
-	console.log("notFResult", notFResult)
 
 	const finalResult = mux16(fResult, notFResult, no)
-	console.log("finalResult", finalResult)
 
 	return finalResult;
 }
